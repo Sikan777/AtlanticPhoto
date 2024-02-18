82ee1e27645e0481a157ab72cdac008ba3fda1fb
Bohdan <133781204+Sikan777@users.noreply.github.com>
2024-02-18 18:42:52 +0200

    Update config.py

--- src/conf/config.py
+++ src/conf/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_URL: str = "postgresql+asyncpg://postgres:11111111!@localhost:5432/postgres"
+    DB_URL: str = "postgresql+asyncpg://postgres:11111111!@localhost:5432/postgres" #random value
     SECRET_KEY_JWT: str = "1234567890"
     ALGORITHM: str = "HS256"
     MAIL_USERNAME: EmailStr = "postgres@meail.com"
